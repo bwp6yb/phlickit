ea33c421b762a198386e58d5a14d87d50457a706
Brandt Parker <bwp6yb@mail.missouri.edu>
2014-11-15 02:32:27 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phlickit::Application.config.secret_key_base = '77be7555d0742085fefccef90da41125433d3d6bdb4ce2192c830213e2da0bdb0ea51ef8176bae9cb628a8e7f43e71b23db71cdb8e073ac7fb78cdbe9fa8eb97'
